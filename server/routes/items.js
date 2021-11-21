137ed6faae94c8d637119dd4ff0c36746040734d
df239 <df239@canterbury.ac.uk>
2021-11-21 15:42:25 +0000

    installed and initialized vue js framework

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var items = [
+  {id:'1', description:"Hello World"},
+  {id:'2', description:"Hello Everyone!"}
+];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('foo bar');
+router.get('/', function(req, res) {
+  res.json(items);
 });
 
 module.exports = router;
