560196b9bcf3383af87b99d08f51323db6e4ad5d
df239 <df239@canterbury.ac.uk>
2021-11-21 17:30:28 +0000

    added post request to server

--- server/routes/items.js
+++ server/routes/items.js
@@ -2,13 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 var items = [
-  {id:'1', description:"Hello World"},
-  {id:'2', description:"Hello Everyone!"}
+  {id:1, description:"Hello World"},
+  {id:2, description:"Hello Everyone!"}
 ];
 
-/* GET users listing. */
+/* GET items listing */
 router.get('/', function(req, res) {
   res.json(items);
+});
+
+/* POST new item */
+router.post('/', function(req, res){
+  var newID = items[items.length - 1].id + 1;
+  items.push({
+    id: newID,
+    description: req.body.description
+  });
+  res.send("successful post");
 });
 
 module.exports = router;
