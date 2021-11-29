255ba1e352a47a68ef488bf32296e86b7b7102be
df239 <df239@canterbury.ac.uk>
2021-11-29 15:36:27 +0000

    passed first two server tests

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var corsOptions = {
+  origin: '*',
+  methods: 'GET,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type',
+  optionsSuccessStatus: 204
+}
+router.options('/', cors(corsOptions));
 
 module.exports = router;
