54094565ee94d309ceafd7e481881c7b6fbb438a
David Filipiak <df239@canterbury.ac.uk>
2021-12-12 16:07:21 +0000

    attempted to add a placeholder image and a webpage icon

--- server/itemsObject.js
+++ server/itemsObject.js
@@ -2,7 +2,7 @@
 var items = {
     1: { id: 1, user_id: "david", description: "My cat",
     lat: 18.684, lon: 148.394, keywords: ["cat","kitten"],
-    image: ""
+    image: "http://placekitten.com/200/300"
     }
   };
 
